19cefe046d5059fc7cdfbf22bc34528472399541
Abhay Agarwal <linkup.abhay@gmail.com>
2020-01-22 15:36:42 +0530

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function(){
+    
+   $('ul').sortable({
+       items:"li:not('.listTitle,.addItem')",
+       connectWith:'ul',
+       dropOnEmpty:true,
+       placeHolder:'emptySpace'
+       
+   });
+    
+    $('input').keydown(function(e) {
+        if(e.keyCode == 13) {
+            var item = $(this).val();
+            
+            $(this).parent().parent().append('<li>' + item + '</li>');
+            $(this).val('');
+        }
+    });
+    
+    $('#trash').droppable({
+        drop: function(event, ui) {
+            ui.draggable.remove();
+        }
+    });
+    
+})
